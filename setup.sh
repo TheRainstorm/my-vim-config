9a6d9a46527f92735217e0163ab746de849fbbef
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2023-09-28 12:45:07 +0800

    add: setup bash

--- setup.sh
+++ setup.sh
@@ -16,13 +16,14 @@ clone_repo () {
 # install dependency
 check_dep () {
     sudo apt update
-    sudo apt install git curl 
-    echo "sudo apt install git curl" 
+    sudo apt install git curl wget
+    echo "sudo apt install git curl wget" 
     return 0
 }
 
 # setup zsh
 setup_zsh () {
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <<EOF
     y
 EOF
@@ -37,6 +38,13 @@ cp basic/.zsh* ~/
 source ~/.zshrc
 }
 
+# setup bash
+setup_bash () {
+echo "[ -f ~/.zsh-custom ] && source ~/.zsh-custom" >> ~/.bashrc
+cp basic/.zsh-custom ~/
+source ~/.bashrc
+}
+
 # setup fzf
 setup_fzf () {
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -45,8 +53,6 @@ y
 y
 y
 EOF
-
-source ~/.zshrc
 }
 
 # tmux
@@ -65,7 +71,14 @@ check_dep
 clone_repo
 cd ~/.my-vim-config
 
-setup_zsh
+if [ $#=2 ]; then
+    if [ $1="bash" ]; then
+	setup_bash
+    fi
+else
+    setup_zsh
+fi
+
 setup_fzf
 setup_tmux
 setup_others
